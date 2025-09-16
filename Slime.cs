d10836f067bf7cc2b7c589026bb2ee0416e6f654
SeishiroNagi291207 <pieroac2912@gmail.com>
2025-09-16 17:51:16 -0500

    zzz

---
+++ Slime.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Slime
+{
+    public int life =10;
+
+    public Slime (int _life)
+    {
+        life = _life;
+    }
+
+    public static Slime operator +(Slime Slime1, Slime Slime2)
+    {
+        return new Slime(Slime1.life + Slime2.life);
+    }
+}
